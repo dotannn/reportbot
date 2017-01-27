a7dfe2e866b304b5e47d96715030cc83a97b11c6
Dotan <dotan1988@gmail.com>
2017-01-27 15:52:30 +0200

    package boilerplate

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup
+
+setup(name='reportbot',
+      version='0.1',
+      description='report machine learning results to you on telegram',
+      url='https://github.com/dotannn/reportbot',
+      author='Dotan Asselmann',
+      author_email='dotan1988@gmail.com',
+      license='MIT',
+      keywords='roc sklearn classification report ml confusion-'
+               ''
+               'git martix',
+      packages=['reportbot'],
+      install_requires=[
+            'telegram',
+      ],
+      zip_safe=False)
